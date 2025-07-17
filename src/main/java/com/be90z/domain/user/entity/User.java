415e5683152fcc1810938704ecb7c5080caaaa56
SaeRyung <sr1094@naver.com>
2025-07-18 05:32:11 +0900

    feat: User Entity 생성

--- src/main/java/com/be90z/domain/user/entity/User.java
+++ src/main/java/com/be90z/domain/user/entity/User.java
@@ -1,4 +1,45 @@
 package com.be90z.domain.user.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "provider", nullable = false, unique = true)
+    private String provider;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "auth", nullable = false)
+    private Auth auth;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender", nullable = false)
+    private Gender gender;
+
+    @Column(name = "birth", nullable = false)
+    private Integer birth;
 }
