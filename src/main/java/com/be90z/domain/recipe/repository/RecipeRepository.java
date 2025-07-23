88d44efc8d9cdc5bdbc05c110f11a69224b1a795
SaeRyung <sr1094@naver.com>
2025-07-24 03:12:20 +0900

    feat: RecipeRepository 생성

---
+++ src/main/java/com/be90z/domain/recipe/repository/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.be90z.domain.recipe.repository;
+
+import com.be90z.domain.recipe.entity.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+}
