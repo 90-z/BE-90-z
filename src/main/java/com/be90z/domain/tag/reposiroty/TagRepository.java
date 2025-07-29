13790d747186481e3406467f85bc1620753f7705
SaeRyung <sr1094@naver.com>
2025-07-29 19:02:17 +0900

    feat: TagRepository 생성

---
+++ src/main/java/com/be90z/domain/tag/reposiroty/TagRepository.java
@@ -0,0 +1,12 @@
+package com.be90z.domain.tag.reposiroty;
+
+import com.be90z.domain.tag.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByTagName(String tagName);
+}
