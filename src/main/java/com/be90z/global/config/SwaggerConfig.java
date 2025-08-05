33c43c92d2470941c5cac97bc9e3cb4e0565fd36
SaeRyung <sr1094@naver.com>
2025-08-06 06:57:08 +0900

    feat: SwaggerConfig 스웨거 테스트 진행 시 JWT 인증 설정 추가

--- src/main/java/com/be90z/global/config/SwaggerConfig.java
+++ src/main/java/com/be90z/global/config/SwaggerConfig.java
@@ -6,6 +6,9 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 
 import java.util.List;
 
@@ -29,6 +32,15 @@ public class SwaggerConfig {
                         new Server()
                                 .url("http://localhost:8080")
                                 .description("개발 서버")
-                ));
+                ))
+//                JWT 인증 설정
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT 토큰을 입력하세요 (Bearer 접두사 없이)")));
     }
 }
